a58050bb0fbfc924f8920caeda9c1f8da8290064
PrashantRajawat <prashantsinghrajawat27@gmail.com>
2023-06-03 10:36:32 +0530

    Design Changes- Cloud and API Key

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import MyUrl from "./myurl";
-import Particles from "./particles";
+import Particles from "./myurl/particles";
 
 
 const inter = Inter({ subsets: ["latin"] });
--- src/pages/myurl/index.jsx
+++ src/pages/myurl/index.jsx
@@ -1,11 +1,9 @@
-import { useState,useEffect } from "react";
-import postUrl from "../api/url";
+import { useState, useEffect } from "react";
 import a from "../../styles/Home.module.css";
-import ParticlesBack from "../particles";
+import ParticlesBack from "./particles";
 export default function MyUrl() {
   const [url, setUrl] = useState();
   const [newUrl, setNewUrl] = useState();
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const encodedParams = new URLSearchParams();
@@ -14,55 +12,63 @@ export default function MyUrl() {
       method: "POST",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
-        "X-RapidAPI-Key": "d925b10bfemsh6fdd5b15f4ed795p1d61d2jsn6c66401d77ed",
+        "X-RapidAPI-Key": `${process.env.NEXT_PUBLIC_APIKEY}`,
         "X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com",
       },
       body: encodedParams,
     };
-    // const endpoint = `${process.env.NEXT_PUBLIC_HOST}`;
-    // const response = await postUrl(endpoint, options);
-    // console.log(response);
 
-    fetch("https://url-shortener-service.p.rapidapi.com/shorten", options)
-      .then((response) => response.json())
-      .then((response) =>{ setNewUrl(response.result_url)})
+    fetch(`${process.env.NEXT_PUBLIC_HOST}`, options)
+      .then((response) => {
+        response.json();
+        console.log(response);
+        setNewUrl(response.result_url);
+      })
       .catch((err) => console.error(err));
-      console.log(newUrl)
   };
   const handleCopyButtonClick = () => {
     navigator.clipboard.writeText(newUrl);
+    alert("Yoohoo!  🎉  Url Copied");
   };
   return (
     <div className={a.homeContainer}>
- <ParticlesBack/>
-      <div className={a.urlContainer}>
-        <div className={a.head}>MyMiniUrl</div>
-        <form onSubmit={handleSubmit} className={a.form}>
-          <input
-            type="text"
-            required
-            placeholder="Paste your url here"
-            onChange={(e) => setUrl(e.target.value)}
-          />
-          <button type="submit">Short Url</button>
-        </form>
-{newUrl==undefined  ? (
-  <>
-  <div className={a.text}>Welcome, click on the button for a new url</div>
-  </>
-) :
-   (
-        <div className={a.form}>
-          <input
-            type="text"
-            disabled
-            placeholder="New url will appear here"
-            value={newUrl}
-          ></input>
-          <button onClick={handleCopyButtonClick}>Copy Url</button>
+      <ParticlesBack />
+      <div className={a.cloud}>
+        <div>
+          <div className={a.head}>MyMiniUrl</div>
+          <form onSubmit={handleSubmit} className={a.form}>
+            <input
+              type="text"
+              required
+              className={a.input}
+              placeholder="Paste your url here"
+              onChange={(e) => setUrl(e.target.value)}
+            />
+
+            <button className={a.button} type="submit">
+              Click Me!
+            </button>
+          </form>
+          {newUrl === undefined ? (
+            <>
+              <div className={a.text}>
+                Welcome, click on the button for a new url !
+              </div>
+            </>
+          ) : (
+            <div className={a.resultData}>
+              <input
+                type="text"
+                disabled
+                value={newUrl}
+                className={a.input}
+              ></input>
+              <button className={a.button} onClick={handleCopyButtonClick}>
+                Copy
+              </button>
+            </div>
+          )}
         </div>
-  )
-}   
       </div>
     </div>
   );
